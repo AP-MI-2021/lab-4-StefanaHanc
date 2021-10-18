e54f37559fad53e6f3070d8475659b93c4e6c701
ceva nume <sadagain>
2021-10-18 16:00:36 +0300

    pr

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ testafisarefaraduplicate()
 
 def afisarea_primelor_n_nr(l,n):
     """
-    Afișarea sumei primelor n numere pozitive din listă, unde n se citește de la tastatură. 
+    Afișarea sumei primelor n numere pozitive din listă, unde n se citește de la tastatură.
     :param l:Lista verifiata
     :param n:Cele n numere citite
-    :return: Suma in caz ca exista n numere pozitiva ,none in caz ca nu exista 
+    :return: Suma in caz ca exista n numere pozitiva ,none in caz ca nu exista
     """
     s=0
     for x in l:
@@ -62,3 +62,60 @@ def test_afisare_primele_n_numere():
     assert afisarea_primelor_n_nr([10,12,2,-2,-2],3)==24
 
 test_afisare_primele_n_numere()
+
+
+def daca_sunt_cres(l):
+    rezultat=[]
+    for x in l:
+        if x>=0:
+            rezultat.append(x)
+    for i in range (len(rezultat)-1):
+        if rezultat[i]>rezultat[i+1]:
+            return False
+    return True
+def test_daca_sunt_cresc():
+    assert daca_sunt_cres([1,-2,4,8,-6]) is True
+    assert daca_sunt_cres([1,-2,4,8,3]) is False
+    assert daca_sunt_cres([-1,34,2,8]) is False
+
+test_daca_sunt_cresc()
+def nr_divizori(n):
+    divizori=0
+    d=2
+    while d<n:
+        if n%d==0:
+            divizori+=1
+        d+=1
+    return divizori
+
+def test_nr_divizori():
+    assert nr_divizori(3)==0
+    assert nr_divizori(25)==1
+
+test_nr_divizori()
+
+def afisare_care_apare_o_singura_data(l):
+    """
+    Afișarea listei obținute din lista inițială în care numerele care apar doar o singură dată sunt
+înlocuite cu numărul de divizori proprii ai numărului.
+    :param l:lista care trebuie verificata
+    :return:Se returneaza o noua lista
+    """
+    rezultat=[]
+    for i in range (len(l)):
+        ok=1
+        for j in range(len(l)):
+            if l[i]==l[j] and i!=j:
+                ok=0
+        if ok==1:
+            rezultat.append(nr_divizori(l[i]))
+        else:
+            rezultat.append(l[i])
+    return rezultat
+
+def test_afisare_care_apare_o_singura_data():
+    assert afisare_care_apare_o_singura_data([25,13,13,19])==[1,13,13,0]
+    assert afisare_care_apare_o_singura_data([3,5,9])==[0,0,1]
+    assert afisare_care_apare_o_singura_data([13,13,13])==[13,13,13]
+
+test_afisare_care_apare_o_singura_data()
